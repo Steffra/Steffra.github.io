993dd91bf1c988c8095962575f783336922ae488
Krausz Bertold <krauszbertold@gmail.com>
2019-09-06 22:12:01 +0200

    more devs for more features

---
+++ main.js
@@ -0,0 +1,21 @@
+//sidebar mouseover actions
+  $('.hovertext, .icon').hover(function(){
+    myarray=["one", "two", "three", "four", "five", ]
+    if(jQuery.inArray($(this).prop('id'), myarray) !== -1){
+      $(this).css("opacity","0");
+      $(this).next().css("opacity","1")
+    }else{
+      $(this).css("opacity","1");
+      $(this).prev().css("opacity","0")
+    }
+  });
+  $('.hovertext, .icon').mouseout(function(){
+    myarray=["one", "two", "three", "four", "five", ]
+    if(jQuery.inArray($(this).prop('id'), myarray) !== -1){
+      $(this).css("opacity","1");
+      $(this).next().css("opacity","0");
+    }else{
+      $(this).css("opacity","0");
+      $(this).prev().css("opacity","1");
+    }
+  });

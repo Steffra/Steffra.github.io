f52fff28a85ef64411b0097b5083cc89659d3653
Krausz Bertold <krauszbertold@gmail.com>
2020-04-26 23:50:48 +0200

    added swiping navigation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $('.hovertext, .icon').hover(function () {
-    myarray = ["one", "two", "three", "four", "five",]
+    myarray = ["index", "skills", "about", "portfolio", "contact",]
     if (jQuery.inArray($(this).prop('id'), myarray) !== -1) {
         $(this).css("opacity", "0");
         $(this).next().css("opacity", "1")
@@ -9,7 +9,7 @@ $('.hovertext, .icon').hover(function () {
     }
 });
 $('.hovertext, .icon').mouseout(function () {
-    myarray = ["one", "two", "three", "four", "five",]
+    myarray = ["index", "skills", "about", "portfolio", "contact",]
     if (jQuery.inArray($(this).prop('id'), myarray) !== -1) {
         $(this).css("opacity", "1");
         $(this).next().css("opacity", "0");
@@ -43,10 +43,83 @@ if (canvas) {
     canvas.height = canvas.width;
 }
 
-$(function(){
-    $( "body" ).on( "swipeleft", swipeleftHandler );
 
-    function swipeleftHandler( event ){
-    window.location.href = "index.php";
+var current_page=$('.active').attr('id');
+
+var gesture = {
+    x: [],
+    y: [],
+    match: ''
+  },
+  tolerance = 100;
+window.addEventListener('touchstart',function(e){
+    for (i=0;i<e.touches.length;i++){
+    var dot = document.createElement('div');
+    dot.id = i
+    dot.style.top = e.touches[i].clientY-25+'px'
+    dot.style.left = e.touches[i].clientX-25+'px'
+    document.body.appendChild(dot)
+    gesture.x.push(e.touches[i].clientX)
+    gesture.y.push(e.touches[i].clientY)
     }
 });
+window.addEventListener('touchmove',function(e){
+    for (var i=0; i<e.touches.length; i++) {
+    var dot = document.getElementById(i);
+    dot.style.top = e.touches[i].clientY-25+'px'
+    dot.style.left = e.touches[i].clientX-25+'px'
+    gesture.x.push(e.touches[i].clientX)
+    gesture.y.push(e.touches[i].clientY)
+    }
+});
+window.addEventListener('touchend',function(e){
+var dots = document.querySelectorAll('div'),
+    xTravel = gesture.x[gesture.x.length-1] - gesture.x[0],
+    yTravel = gesture.y[gesture.y.length-1] - gesture.y[0];
+if (yTravel<tolerance && yTravel>-tolerance && xTravel<-tolerance){
+    switch ($('.active').attr('id')){
+        case'index':
+            window.location.href = "about";
+        break;
+        case'about':
+            window.location.href = "skills";
+        break;
+        case'skills':
+            window.location.href = "portfolio";
+        break;
+        case'portfolio':
+            window.location.href = "contact";
+        break;
+        case'contact':
+        break;
+    };
+}
+if (yTravel<tolerance && yTravel>-tolerance && xTravel>tolerance){
+    switch ($('.active').attr('id')){
+        case'index':
+        break;
+        case'about':
+            window.location.href = "index";
+        break;
+        case'skills':
+            window.location.href = "about";
+        break;
+        case'portfolio':
+            window.location.href = "skills";
+        break;
+        case'contact':
+            window.location.href = "portfolio";
+        break;
+    };
+}
+if (gesture.match!==''){
+}
+
+gesture.x = []
+gesture.y = []
+gesture.match = xTravel = yTravel = ''
+for (i=0;i<dots.length;i++){
+  dots[i].id = ''
+  
+}
+})

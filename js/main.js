cd1206f6e155721a59a6189e0b5ea6787d6c1200
Krausz Bertold <krauszbertold@gmail.com>
2020-05-06 23:43:49 +0200

    refreshed the way swiping works on navigation

--- js/main.js
+++ js/main.js
@@ -43,83 +43,72 @@ if (canvas) {
     canvas.height = canvas.width;
 }
 
-
 var current_page=$('.active').attr('id');
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
-var gesture = {
-    x: [],
-    y: [],
-    match: ''
-  },
-  tolerance = 30;
-window.addEventListener('touchstart',function(e){
-    for (i=0;i<e.touches.length;i++){
-    var dot = document.createElement('div');
-    dot.id = i
-    dot.style.top = e.touches[i].clientY-25+'px'
-    dot.style.left = e.touches[i].clientX-25+'px'
-    document.body.appendChild(dot)
-    gesture.x.push(e.touches[i].clientX)
-    gesture.y.push(e.touches[i].clientY)
-    }
-});
-window.addEventListener('touchmove',function(e){
-    for (var i=0; i<e.touches.length; i++) {
-    var dot = document.getElementById(i);
-    dot.style.top = e.touches[i].clientY-25+'px'
-    dot.style.left = e.touches[i].clientX-25+'px'
-    gesture.x.push(e.touches[i].clientX)
-    gesture.y.push(e.touches[i].clientY)
-    }
-});
-window.addEventListener('touchend',function(e){
-var dots = document.querySelectorAll('div'),
-    xTravel = gesture.x[gesture.x.length-1] - gesture.x[0],
-    yTravel = gesture.y[gesture.y.length-1] - gesture.y[0];
-if (yTravel<tolerance && yTravel>-tolerance && xTravel<-tolerance){
-    switch ($('.active').attr('id')){
-        case'index':
-            window.location.href = "about";
-        break;
-        case'about':
-            window.location.href = "skills";
-        break;
-        case'skills':
-            window.location.href = "portfolio";
-        break;
-        case'portfolio':
-            window.location.href = "contact";
-        break;
-        case'contact':
-        break;
-    };
-}
-if (yTravel<tolerance && yTravel>-tolerance && xTravel>tolerance){
-    switch ($('.active').attr('id')){
-        case'index':
-        break;
-        case'about':
-            window.location.href = "index";
-        break;
-        case'skills':
-            window.location.href = "about";
-        break;
-        case'portfolio':
-            window.location.href = "skills";
-        break;
-        case'contact':
-            window.location.href = "portfolio";
-        break;
-    };
-}
-if (gesture.match!==''){
-}
+var xDown = null;                                                        
+var yDown = null;
 
-gesture.x = []
-gesture.y = []
-gesture.match = xTravel = yTravel = ''
-for (i=0;i<dots.length;i++){
-  dots[i].id = ''
-  
-}
-})
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            switch ($('.active').attr('id')){
+                case'index':
+                    window.location.href = "about";
+                break;
+                case'about':
+                    window.location.href = "skills";
+                break;
+                case'skills':
+                    window.location.href = "portfolio";
+                break;
+                case'portfolio':
+                    window.location.href = "contact";
+                break;
+                case'contact':
+                break;
+            };
+        } else {
+            switch ($('.active').attr('id')){
+                case'index':
+                break;
+                case'about':
+                    window.location.href = "index";
+                break;
+                case'skills':
+                    window.location.href = "about";
+                break;
+                case'portfolio':
+                    window.location.href = "skills";
+                break;
+                case'contact':
+                    window.location.href = "portfolio";
+                break;
+            };
+        }                       
+    } 
+    xDown = null;
+    yDown = null;                                             
+};
